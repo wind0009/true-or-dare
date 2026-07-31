affb337640f4a55369b0e365a821c77914991f73
Windson <windsomdabire@gmail.com>
2026-07-31 08:09:31 +0000

    Prepare Android APK project

---
+++ android/app/src/main/java/com/wind0009/actionverite/MainActivity.java
@@ -0,0 +1,5 @@
+package com.wind0009.actionverite;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wind0009.actionverite',
+  appName: 'Action ou Vérité',
+  webDir: 'dist',
+};
+
+export default config;
